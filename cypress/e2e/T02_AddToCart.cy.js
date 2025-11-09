74509b9546921aa08726db59f93671fe8ffbbd00
Ibrahim Fahmy <ibrahimmohamedfahmi1@gmail.com>
2025-11-09 20:04:18 +0200

    add  the sad scenarios for add to cart flow

--- cypress/e2e/T02_AddToCart.cy.js
+++ cypress/e2e/T02_AddToCart.cy.js
@@ -114,6 +114,31 @@ describe("Add to Cart Flow", function()
                     expect(ProductName1).to.be.eq(ProductName);
                 });
         });
+
+        context("Sad Scenarios", function()
+        {
+            it("should redirect unauthenticated user to login page", function()
+            {
+                // step 1: user try to access products page during the url
+                cy.visit("https://www.saucedemo.com/inventory.html", { failOnStatusCode: false });
+
+                // step 2: check the user will redirect to sign in page
+                cy.url().should("eq", "https://www.saucedemo.com/");
+            });
+              
+            it("should not allow adding product without login", function()
+            {
+                // step 1: user try to access product page during the url
+                cy.visit("https://www.saucedemo.com/inventory-item.html?id=4", { failOnStatusCode: false });
+
+                // step 2: verify that the add to cart button not exists 
+                cy.get("#add-to-cart").should("not.exist");
+
+                // step 3: check the user will redirect to sign in page
+                 cy.url().should("eq", "https://www.saucedemo.com/");
+            });
+              
+        });
     });
 
     
