1352e2ba8a51bea652f41b9ea3be458a156354f5
Ibrahim Fahmy <ibrahimmohamedfahmi1@gmail.com>
2025-11-11 14:58:20 +0200

    add three sad scenarios for checkout flow

--- cypress/e2e/T03_CheckOut.cy.js
+++ cypress/e2e/T03_CheckOut.cy.js
@@ -122,6 +122,185 @@ describe("Add to Cart Flow", function()
 
     context("Sad Scenarios", function()
     {   
+        it("verify that the user can not go to the checkout page without the first name in the step one", function()
+        {
+             // step 1: verify that the actual base url match the expected url for sign in page
+             cy.checkUrl("https://www.saucedemo.com/");
+
+             // step 2: get the user name text filed and type a valid user name
+             cy.get('[data-test="username"]').type(userData.UserName);
+ 
+             // step 3: get the password text filed and type a valid password
+             cy.get('[data-test="password"]').type(userData.Password);
+ 
+             // step 4: verify that the login button is visible and click on it
+             cy.get('[data-test="login-button"]').should("be.visible").click();
+             
+             // step 5: verify that the user reach the home page
+             cy.checkUrl("https://www.saucedemo.com/inventory.html");
+ 
+             // step 6: check the logo's title in the add to cart page
+             cy.xpath("//div[text()='Swag Labs']").should("contain", "Swag Labs");
+              
+             // step 8: add that product to the cart
+             cy.get("#add-to-cart-sauce-labs-backpack").should("be.visible").click();
+ 
+             //step 9: go to cart page 
+             cy.get(".shopping_cart_link").should("be.visible").click();
+ 
+             // step 10: check the url of the cart page
+             cy.checkUrl("https://www.saucedemo.com/cart.html");
+ 
+             // step 11: get the checkout button and verify it is visible and its name is "Checkout"
+             cy.get("#checkout").should("be.visible").then((ButtonName)=>
+                 {
+                     expect(ButtonName.text()).to.be.equal("Checkout");
+                 });
+             // step 12: verify that the checkout button redirect to the checkout page step one
+             cy.get("#checkout").click();
+ 
+             // step 13: check the url for checkout page
+             cy.checkUrl("https://www.saucedemo.com/checkout-step-one.html");
+ 
+             // step 14: get the text filed for the first name and check it visible
+             cy.get("#first-name").should("be.visible");
+ 
+             // step 15: get the text filed for the last name and type a valid last name
+             cy.get("#last-name").type(userData.LastName);
+
+             // step 16: get the text filed for the post code and and type a valid post code
+             cy.get("#postal-code").type(userData.PostNumber);
+
+             // step 17: verify that the continue button is visible and click in it
+            cy.get("#continue").should("be.visible").click();
+
+            // step 18: get the validation for the first name text filed
+            cy.xpath("//h3[@data-test='error']").then((validation)=>
+                {
+                    expect(validation.text()).to.be.equal("Error: First Name is required");
+                });
+ 
+        });
+
+        it("verify that the user can not go to the checkout page without the last name in the step one", function()
+        {
+             // step 1: verify that the actual base url match the expected url for sign in page
+             cy.checkUrl("https://www.saucedemo.com/");
+
+             // step 2: get the user name text filed and type a valid user name
+             cy.get('[data-test="username"]').type(userData.UserName);
+ 
+             // step 3: get the password text filed and type a valid password
+             cy.get('[data-test="password"]').type(userData.Password);
+ 
+             // step 4: verify that the login button is visible and click on it
+             cy.get('[data-test="login-button"]').should("be.visible").click();
+             
+             // step 5: verify that the user reach the home page
+             cy.checkUrl("https://www.saucedemo.com/inventory.html");
+ 
+             // step 6: check the logo's title in the add to cart page
+             cy.xpath("//div[text()='Swag Labs']").should("contain", "Swag Labs");
+              
+             // step 8: add that product to the cart
+             cy.get("#add-to-cart-sauce-labs-backpack").should("be.visible").click();
+ 
+             //step 9: go to cart page 
+             cy.get(".shopping_cart_link").should("be.visible").click();
+ 
+             // step 10: check the url of the cart page
+             cy.checkUrl("https://www.saucedemo.com/cart.html");
+ 
+             // step 11: get the checkout button and verify it is visible and its name is "Checkout"
+             cy.get("#checkout").should("be.visible").then((ButtonName)=>
+                 {
+                     expect(ButtonName.text()).to.be.equal("Checkout");
+                 });
+             // step 12: verify that the checkout button redirect to the checkout page step one
+             cy.get("#checkout").click();
+ 
+             // step 13: check the url for checkout page
+             cy.checkUrl("https://www.saucedemo.com/checkout-step-one.html");
+ 
+             // step 14: get the text filed for the first name and type a valid first name
+             cy.get("#first-name").type(userData.FirstName);
+
+             // step 15: get the text filed for the last name and check it visible
+             cy.get("#last-name").should("be.visible");
+
+             // step 16: get the text filed for the post code and and type a valid post code
+             cy.get("#postal-code").type(userData.PostNumber);
+
+             // step 17: verify that the continue button is visible and click in it
+            cy.get("#continue").should("be.visible").click();
+
+            // step 18: get the validation for the first name text filed
+            cy.xpath("//h3[@data-test='error']").then((validation)=>
+                {
+                    expect(validation.text()).to.be.equal("Error: Last Name is required");
+                });
+ 
+        });
+
+        it("verify that the user can not go to the checkout page without the post code in the step one", function()
+        {
+             // step 1: verify that the actual base url match the expected url for sign in page
+             cy.checkUrl("https://www.saucedemo.com/");
+
+             // step 2: get the user name text filed and type a valid user name
+             cy.get('[data-test="username"]').type(userData.UserName);
+ 
+             // step 3: get the password text filed and type a valid password
+             cy.get('[data-test="password"]').type(userData.Password);
+ 
+             // step 4: verify that the login button is visible and click on it
+             cy.get('[data-test="login-button"]').should("be.visible").click();
+             
+             // step 5: verify that the user reach the home page
+             cy.checkUrl("https://www.saucedemo.com/inventory.html");
+ 
+             // step 6: check the logo's title in the add to cart page
+             cy.xpath("//div[text()='Swag Labs']").should("contain", "Swag Labs");
+              
+             // step 8: add that product to the cart
+             cy.get("#add-to-cart-sauce-labs-backpack").should("be.visible").click();
+ 
+             //step 9: go to cart page 
+             cy.get(".shopping_cart_link").should("be.visible").click();
+ 
+             // step 10: check the url of the cart page
+             cy.checkUrl("https://www.saucedemo.com/cart.html");
+ 
+             // step 11: get the checkout button and verify it is visible and its name is "Checkout"
+             cy.get("#checkout").should("be.visible").then((ButtonName)=>
+                 {
+                     expect(ButtonName.text()).to.be.equal("Checkout");
+                 });
+             // step 12: verify that the checkout button redirect to the checkout page step one
+             cy.get("#checkout").click();
+ 
+             // step 13: check the url for checkout page
+             cy.checkUrl("https://www.saucedemo.com/checkout-step-one.html");
+ 
+             // step 14: get the text filed for the first name and type a valid first name
+             cy.get("#first-name").type(userData.FirstName);
+
+             // step 15: get the text filed for the last name and type a valid last name
+             cy.get("#last-name").type(userData.LastName);
+
+             // step 16: get the text filed for the post code and check it is visible
+             cy.get("#postal-code").should("be.visible");
+
+             // step 17: verify that the continue button is visible and click in it
+            cy.get("#continue").should("be.visible").click();
+
+            // step 18: get the validation for the first name text filed
+            cy.xpath("//h3[@data-test='error']").then((validation)=>
+                {
+                    expect(validation.text()).to.be.equal("Error: Postal Code is required");
+                });
+ 
+        });
 
     });
    
